b064856fec4835ec63b28d7d8128d481af8a7598
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-19 22:59:02 +0300

    Add isDigit function

--- alternative_syntax_calculator.js
+++ alternative_syntax_calculator.js
@@ -2,4 +2,6 @@ function isOperator(opr) {
     return opr === "+" || opr === "-" || opr === "*" || opr === "/" || opr === "%";
 }
 
-
+function isDigit(num) {
+    return num >= "0" && num <= "9";
+}
