7fd26ff91db7b009fcf1871af15dca482c6d842a
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-19 22:59:48 +0300

    Add calculator function

--- alternative_syntax_calculator.js
+++ alternative_syntax_calculator.js
@@ -21,3 +21,32 @@ function calculate(operand1, operand2, operator) {
     }
 }
 
+
+function calculator(expression) {
+    const tokens = expression.split(" ");
+    console.log("splitted expr: ", tokens);
+    console.log("reversed expr: ", tokens.reverse());
+    
+    let i = 0;
+    while (tokens.length > 1) {
+        if (!isOperator(tokens[i]))
+        if (isOperator(tokens[i]) && isOperator(tokens[i+1])) {
+            i += 1;
+        } else if (isOperator(tokens[i]) && isDigit(tokens[i+1]) && isDigit(tokens[i+2])) {
+            const operand1 = parseFloat(tokens[i+1]);
+            const operand2 = parseFloat(tokens[i+2]);
+            const operator = tokens[i];
+            
+            const result = calculate(operand1, operand2, operator);
+            tokens.splice(i, 3, result);
+            i = 0;
+        }   
+       
+        else { throw new TypeError("Invalid expression!"); }
+        
+        console.log("Tokens: ", tokens);
+    }
+    
+    return tokens.pop();
+}
+
