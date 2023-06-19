2e375276946dd07054e1568d53afc824bde9e255
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-19 22:59:25 +0300

    Add calculate function

--- alternative_syntax_calculator.js
+++ alternative_syntax_calculator.js
@@ -5,3 +5,19 @@ function isOperator(opr) {
 function isDigit(num) {
     return num >= "0" && num <= "9";
 }
+
+function calculate(operand1, operand2, operator) {
+    switch (operator) {
+        case "+":
+            return operand1 + operand2;
+        case "-":
+            return operand1 - operand2;
+        case "*":
+            return operand1 * operand2;
+        case "/":
+            return operand1 / operand2;
+        case "%":
+            return operand1 % operand2;
+    }
+}
+
