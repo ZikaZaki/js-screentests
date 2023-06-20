cafa882b7aad14bea3bc42036b4e6ae43a346d93
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-21 00:20:45 +0300

    Add altClaculator function

--- alternative_syntax_calculator.js
+++ alternative_syntax_calculator.js
@@ -23,3 +23,35 @@ function isDigit(str) {
   */
   return /^-?\d+$/.test(str);
 }
+/* ******************************************************************************************************************** */
+function altCalculator(expression) {
+  const tokens = expression.split(" ");
+  let operand1, operand2, operator, result;
+  let i = 0;
+
+  while (i < tokens.length && tokens.length >= 3) {
+    // console.log("length: ", tokens.length);
+    // console.log("index : ", i);
+    if (isOperator(tokens[i]) && isDigit(tokens[i+1]) && isDigit(tokens[i+2])) {
+      operator = tokens[i];
+      operand1 = Number(tokens[i+1]);
+      operand2 = Number(tokens[i+2]);
+      result = calculate(operand1, operand2, operator);
+      tokens.splice(i, 3, result);
+      i = 0;
+      // console.log("length : ", tokens.length);
+      console.log("remaining tokens: ", tokens);
+    } else {i+=1;}
+  }
+
+  // Perform final calcuation if remaining.
+  while (tokens.length > 1) {
+    operand1 = Number(tokens[0]);
+    operator = tokens[1];
+    operand2 = Number(tokens[2]);
+    result = calculate(operand1, operand2, operator);
+    tokens.splice(0, 3, result);
+  }
+
+  return tokens.pop();
+}
