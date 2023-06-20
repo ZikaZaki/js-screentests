85c64af35d26836313f5dc532de956792fdc1280
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-20 23:58:05 +0300

    Add isOperator helper function

--- semantic_calculator.js
+++ semantic_calculator.js
@@ -10,3 +10,7 @@ function isCloseParentheses(char) {
 function isDigit(char) {
   return char >= '0' && char <= '9';
 }
+
+function isOperator(opr) {
+  return opr === "+" || opr === "-" || opr === "*" || opr === "/" || opr === "%";
+}
