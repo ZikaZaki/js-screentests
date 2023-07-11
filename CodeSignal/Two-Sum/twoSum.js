30101910081634a12d14cd00e9179f541a0707ff
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-12 02:11:21 +0300

    Add twoSum function

--- CodeSignal/Two-Sum/twoSum.js
+++ CodeSignal/Two-Sum/twoSum.js
@@ -1,1 +1,12 @@
-
+const twoSum = (nums, target) => {
+  const complements = new Map();
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (complements.has(complement)) {
+      result.push([complements.get(complement), i]);
+    }
+    complements.set(nums[i], i);
+  }
+  return result;
+}
