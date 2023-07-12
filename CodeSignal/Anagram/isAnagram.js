b546ba1b293fe8fe2e02c34c2617ddbfe2c2bfd3
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-13 02:06:20 +0300

    Update isAnagram.js

--- CodeSignal/Anagram/isAnagram.js
+++ CodeSignal/Anagram/isAnagram.js
@@ -1,1 +1,14 @@
+// ===================NEW SOLUTION==========================
+function isAnagram(s, t) {
+    if (s.length !== t.length) return false;
 
+    let sCount = s[0].charCodeAt(0);
+    let tCount = t[0].charCodeAt(0);
+
+    for (let i = 1; i < s.length; i++) {
+        sCount *= s[i].charCodeAt(0);
+        tCount *= t[i].charCodeAt(0);
+    }
+
+    return sCount === tCount;
+}
