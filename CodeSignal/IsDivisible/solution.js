049fe9c4106f3e280835389c69a6e7f186c75b95
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-14 02:36:13 +0300

    Update solution.js

--- CodeSignal/IsDivisible/solution.js
+++ CodeSignal/IsDivisible/solution.js
@@ -5,11 +5,16 @@ function isDivisible(num, k) {
 function solution(a, k) {
     let count = 0;
     
-    for (let i = 0; i < a.length; i++) {
-        for (let j = i+1; j < a.length; j++) {
-            count += isDivisible(a[i] + a[j], k) ? 1 : 0;
+    while (a.length > 1) {
+        for (let j = 1; j < a.length; j++) {
+            count += ((a[0] + a[j]) % k === 0) ? 1 : 0;
+            // count += isDivisible(a[0] + a[j], k) ? 1 : 0;
+            // console.log(`index:${j} Divisible:${(arr[0] + arr[j]) % k === 0}`);
         }
+        
+        a = a.splice(1);
     }
     
     return count;
 }
+
